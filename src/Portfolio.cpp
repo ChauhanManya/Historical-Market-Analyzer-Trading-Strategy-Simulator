66861ed193c8ea3fd25791f0dee6a57d0fa8f70d
ChauhanManya <manya.chauhan@stonybrook.edu>
2026-05-09 21:58:02 -0400

    Finished Portfolio

--- src/Portfolio.cpp
+++ src/Portfolio.cpp
@@ -60,8 +60,9 @@ void Portfolio::sellShares(const string& ticker, int shares, double price, const
             cashBalance += price*shares;
         }
         i++;
-    }while( i< holdings.size() && !found)
+    }while( i< holdings.size() && !found);
 }
+
 void Portfolio::undoLastTrade(){
     TradeRecord r = tradeHistory.pop();
     if(r.action == "SELL"){
@@ -75,15 +76,106 @@ void Portfolio::queueOrder(const Order& order){
 }
 void Portfolio::executeNextOrder(double currentPrice, const string& date){
     Order o = pendingOrders.dequeue();
-    
+    if(o.side == "SELL"){
+        if(o.type == "LIMIT" && currentPrice >= o.targetPrice)
+            sellShares(o.ticker, o.shares, o.targetPrice, date);
+        else if (o.type == "MARKET")
+            sellShares(o.ticker, o.shares, currentPrice, date);
+    }else{//BUY
+        if(o.type == "LIMIT" && currentPrice <= o.targetPrice)
+            buyShares(o.ticker, o.shares, o.targetPrice, date);
+        else if(o.type == "MARKET")
+            buyShares(o.ticker, o.shares, currentPrice, date);
+    }
 }
-double getTotalMarketValue() const;
-double getTotalValue() const;
-double getTotalUnrealizedReturn() const;
-double getCashBalance() const;
-void updatePrice(const string& ticker, double newPrice);
-void sortHoldingsByUnrealizedReturn();  // descending: best performer first
-void sortHoldingsByTicker();            // alphabetical ascending
-void printHoldings()     const;
-void printTradeHistory() const;   // delegates to tradeHistory.printAll()
-void printPendingOrders() const;  // delegates to pendingOrders.printAll()
+double Portfolio::getTotalMarketValue() const{
+    int i = 0;
+    double sum = 0;
+    do{
+        sum += holdings[i].currentPrice * holdings[i].shares;
+        i++;
+    }while( i < holdings.size());
+    return sum;
+}
+
+double Portfolio::getTotalValue() const{
+    return getTotalMarketValue() + cashBalance;
+}
+double Portfolio::getTotalUnrealizedReturn() const{
+    double marketValue = getTotalMarketValue() ;
+    double totalCostBasis;
+    int i =0, sum = 0;
+    do{
+        sum += holdings[i].avgCostBasis * holdings[i].shares;
+        i++;
+    }while( i < holdings.size());
+    return 100*(marketValue - totalCostBasis)/totalCostBasis;
+}
+double Portfolio::getCashBalance() const{
+    return cashBalance;
+}
+void Portfolio::updatePrice(const string& ticker, double newPrice){
+    int i = 0;
+    bool found = false;
+    do{
+        if(holdings[i].ticker == ticker){
+            holdings[i].currentPrice = newPrice;
+            found = true;
+        }
+        i++;
+    }while( i < holdings.size() && !found);
+}
+void Portfolio::sortHoldingsByUnrealizedReturn(){
+    int n = holdings.size();
+    bool swapped;
+  
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            double marketValue_j, marketValue_i, unrealizedValue_i, unrealizedValue_j, totalCostBasis_i, totalCostBasis_j  ;
+            marketValue_j = holdings[j].currentPrice;
+            marketValue_i = holdings[i].currentPrice;
+            totalCostBasis_j = holdings[j].avgCostBasis * holdings[j].shares;
+            totalCostBasis_i = holdings[i].avgCostBasis * holdings[i].shares;
+            unrealizedValue_j = 100*(marketValue_j - totalCostBasis_j)/totalCostBasis_j;
+            unrealizedValue_i = 100*(marketValue_i - totalCostBasis_i)/totalCostBasis_i;
+            if (unrealizedValue_i < unrealizedValue_j) {
+                swap(holdings[j], holdings[j + 1]);
+                swapped = true;
+            }
+        }
+        // If no two elements were swapped, then break
+        if (!swapped)
+            break;
+    }
+} 
+void Portfolio::sortHoldingsByTicker(){
+    int n = holdings.size();
+    bool swapped;
+  
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (holdings[j].ticker < holdings[j + 1].ticker) {
+                swap(holdings[j], holdings[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}       
+void Portfolio::printHoldings()     const{
+    int i =0;
+    cout << "Ticker |" << "| No. of Shares |" << " | Current Price | " << " | Avg. Cost Bases" << endl ;
+    do{
+       cout << holdings[i].ticker << holdings[i].shares << holdings[i].currentPrice << holdings[i].avgCostBasis << endl;
+        i++;
+    }while( i < holdings.size());    
+}
+void Portfolio::printTradeHistory() const{
+    tradeHistory.printAll();
+} 
+void Portfolio::printPendingOrders() const{
+    pendingOrders.printAll();
+}
