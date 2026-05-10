65b10dac362fa64a9140a512ad3d3bee076a6f53
ChauhanManya <manya.chauhan@stonybrook.edu>
2026-05-09 23:06:46 -0400

    Completed all functions of this file

--- src/TradingStrategy.cpp
+++ src/TradingStrategy.cpp
@@ -1,18 +1,25 @@
-// struct SimResult {
-// string strategyName;
-// double finalValue;
-// double totalInvested;
-// double totalReturn; // percentage
-// double cagr; // Compound Annual Growth Rate
-// double maxDrawdown; // worst peak-to-trough drop
-// int totalTrades;
-// };
-//  Pure virtual: virtual SimResult backtest(PriceHistory* history, double
-// monthlyCapital, int startYear, int endYear) = 0
-//  Pure virtual: virtual string getName() const = 0
-//  Non-virtual: void printResult(const SimResult& result) const
-//  Non-virtual helper: double calculateCAGR(double startVal, double endVal,
-// int years) const
-//  Non-virtual helper: double calculateMaxDrawdown(const vector<double>&
-// portfolioValues) const
-//  Virtual destructor
+#include <iostream>
+#include "TradingStrategy.h";
+#include <math.h>
+using namespace std;
+
+void TradingStrategy::printResult(const SimResult& result) const{
+    cout << result.strategyName << result.totalInvested << result.totalReturn << result.totalTrades <<  result.maxDrawdown << result.cagr << result.finalValue << endl;  ;
+}
+double TradingStrategy::calculateCAGR(double startVal, double endVal, int years) const{
+    if (years <= 0 || startVal <= 0) return 0.0;
+    return (pow( endVal / startVal,1.0/years ) - 1)*100;
+}
+double TradingStrategy::calculateMaxDrawdown(const vector<double>& portfolioValues) const{
+    double max = pow(2,-53), min = pow(2,53);
+    int i = 0;
+    do{
+        if (portfolioValues[i] > max )
+            max = portfolioValues[i];
+        else if (portfolioValues[i] < min )
+            min = portfolioValues[i];
+        i++;
+    }while( i < portfolioValues.size());
+
+    return abs(100*(max - min)/max);
+}
