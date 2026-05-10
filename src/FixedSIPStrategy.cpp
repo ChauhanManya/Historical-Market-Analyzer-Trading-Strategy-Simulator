717a7f42bac7c84e47204b873c125e70541a2d0b
ChauhanManya <manya.chauhan@stonybrook.edu>
2026-05-10 00:05:56 -0400

    backtest in progress

--- src/FixedSIPStrategy.cpp
+++ src/FixedSIPStrategy.cpp
@@ -1,3 +1,32 @@
-// Invests a fixed dollar amount on the first trading day of each month. - backtest(...) —
-// iterate through PriceHistory month by month, buy fractional shares at close price -
-// getName() — returns “Fixed SIP”
+#include "FixedSIPStrategy.h"
+#include "PriceHistory.h"
+#include "Portfolio.h"
+#include <string>
+using namespace std;
+
+FixedSIPStrategy::FixedSIPStrategy() {}
+
+SimResult FixedSIPStrategy::backtest(PriceHistory* history, double monthlyCapital, int startYear, int endYear) override{
+    SimResult s;
+    s.strategyName = "Fixed SIP";
+    Portfolio owner("Manya", 10000);
+    string date =  startYear + "-01-01" ;
+    PriceNode* p = history->findByDate(date); 
+
+    for( int i = startYear; i <= endYear ; i++){
+        for( int j = 1; j <= 12 ; j++){
+            owner.cashBalance += monthlyCapital;
+            string date = to_string(i) + "-" + to_string(j) + "-01";
+            double closePrice = p->close;
+            int shares = monthlyCapital / closePrice;
+            owner.buyShares("AMZN", shares, closePrice, date);
+            p = p->next;
+        }
+        
+    }
+}
+
+
+string FixedSIPStrategy::getName() const override{
+    return "FIXED SIP";
+}
